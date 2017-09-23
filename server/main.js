2094756ec7da3b64f35f1a007a5c3120f455e2af
sl <tyu.sam@gmail.com>
2017-09-23 19:46:40 -0400

    Added a watcher that moves people to the correct part of the queue.

--- server/main.js
+++ server/main.js
@@ -4,11 +4,11 @@ const port = 8080;
 const server = require('./src/server.js');
 
 const colorutils = require('./src/util/colorutil.js');
-
-require('./src/util/linemanager.js')(); // start the line manager
+const linemanager = require('./src/util/linemanager.js');
 
 const main = async function main() {
   colorutils.initializeAllExisting();
+  linemanager.setup();
   server.listen(port);
   console.log(`successfully started server, listening on port ${port}`);
 };
