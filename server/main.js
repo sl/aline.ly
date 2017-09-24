b71986c09556156210756b1ad755f34f7b722967
sl <tyu.sam@gmail.com>
2017-09-24 08:11:15 -0400

    Added port overwriting.

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const port = 8080;
+let port = 8080;
 const server = require('./src/server.js');
 
 const colorutils = require('./src/util/colorutil.js');
@@ -10,7 +10,8 @@ const main = async function main() {
   colorutils.initializeAllExisting();
   linemanager.setup();
   if (process.env.CI_ENV === 'PRODUCTION') {
-    server.listen('0.0.0.0', 80);
+    port = 80;
+    server.listen('0.0.0.0', port);
   } else {
     server.listen(port);
   }
