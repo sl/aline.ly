5a48038b99000f99e4c4e7590c3655de61712fba
Carol Chen <carolyfisher@gmail.com>
2017-09-24 11:17:55 -0400

    Update login

--- server/main.js
+++ server/main.js
@@ -9,7 +9,7 @@ const linemanager = require('./src/util/linemanager.js');
 const main = async function main() {
   colorutils.initializeAllExisting();
   linemanager.setup();
-  if (true) {
+  if (false) {
     port = 80;
     server.listen(port, '0.0.0.0');
   } else {
