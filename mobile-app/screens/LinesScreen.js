41ac0d84c41ca6bf507ef538e924033e7976b8d2
Carol Chen <carolyfisher@gmail.com>
2017-09-24 04:39:47 -0400

    This is stable and works, if something breaks very badly in the futured come here

--- mobile-app/screens/LinesScreen.js
+++ mobile-app/screens/LinesScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, Container, Image, Button, ScrollView, Dimensions, TextInput, Alert } from 'react-native';
+import { StyleSheet, Text, View, Container, Image, Button, ScrollView, Dimensions, TextInput, Alert, ListView, FlatList, ListItem } from 'react-native';
 
 const config = require('../config.json');
 
 import firebase from 'firebase';
+import Line from '../components/Line';
+import ReadyLine from '../components/ReadyLine';
 
 export default class LinesScreen extends React.Component {
     static navigationOptions = {
@@ -11,83 +13,153 @@ export default class LinesScreen extends React.Component {
     };
 
     constructor(props){
-    	super(props);
-    	this.state = {
+        super(props);
+        this.state = {
             lineCode: "",
             signedIn: firebase.auth().currentUser,
-    	};
-        this.db = firebase.database()
+            data: [],
+            alerts: []
+        };
+        this.joinLine = this.joinLine.bind(this);
     }
 
-  joinLine(code){
-      queryRef = firebase.database().ref("/server/lines/").orderByChild("line_code").equalTo(code)
-      queryRef.once('value').then(function(snap) {
-        if (snap.hasChild(code)){
-              var userId = firebase.auth().currentUser.uid
-              addUserRef = firebase.database().ref("/server/lines/" + code + "/in_line/");
-              addUserRef.on('value', function(snapshot) {
-                if (snapshot.hasChild(userId)) {
-                    Alert.alert(
-                        'Oops! ',
-                        "You're already in this line. Be patient, and try to do something exciting!",
-                        [
-                            {text: 'OK'},
-                        ],
-                        { cancelable: false }
-                    )
-                } else{
-                    var key = firebase.database().ref("/server/lines/" + code + "/in_line/").child(userId).key;
-                    var updates = {};
-                    updates["/server/lines/" + code + "/in_line/" + key] = new Date().getTime();
+    joinLine(code){
+        queryRef = firebase.database().ref("/server/lines/").orderByChild("line_code").equalTo(code);
+        let ctx = this;
+        queryRef.once('value').then(function(snap) {
+            if (snap.hasChild(code)){
+                var userId = firebase.auth().currentUser.uid
+                addUserRef = firebase.database().ref("/server/lines/" + code + "/in_line/");
+                addUserRef.once('value', function(snapshot) {
+                    if (snapshot.hasChild(userId)) {
+                        Alert.alert(
+                            'Hold!',
+                            "You're already in this line. Don't stay here checking your app all the time!",
+                            [
+                                {text: 'OK'},
+                            ],
+                            { cancelable: false }
+                        )
+                    } else{
+                        firebase.database().ref("/server/lines/" + code).once('value').then(function(snapshot) {
+                            if (snapshot.hasChild('in_line')){
 
-                    firebase.database().ref().update(updates);
+                                var key = firebase.database().ref("/server/lines/" + code + "/in_line/").child(userId).key;
+                                var updates = {};
+                                updates["/server/lines/" + code + "/in_line/" + key] = new Date().getTime();
+                                firebase.database().ref().update(updates);
+                                Alert.alert(
+                                    'Congrats!',
+                                    "You've just been added to the queue!",
+                                    [
+                                        {text: 'OK'},
+                                    ],
+                                    { cancelable: false }
+                                )
+
+                            } else {
+                                var key = firebase.database().ref("/server/lines/" + code + '/').child('in_line').key;
+                                var updates = {};
+
+                                updates["/server/lines/" + code + "/" + key + '/' + userId + '/'] = new Date().getTime()
+                                firebase.database().ref().update(updates);
+                                Alert.alert(
+                                    'Congrats!',
+                                    "You've just been added to the queue!",
+                                    [
+                                        {text: 'OK'},
+                                    ],
+                                    { cancelable: false }
+                                )
+
+                            }
+                        });
+                    }
+                });
+
+            }
+            else {
+                Alert.alert(
+                    'Uh oh!',
+                    "Looks like that line doesn't exist. Try again!",
+                    [
+                        {text: 'OK'},
+                    ],
+                    { cancelable: false }
+                )
+            }
+        });
+    }
+    listenForItems(code){
+        let ctx = this;
+        firebase.database().ref().child('/server/lines/').on('value', (snap) => {
+            var listOfItems = []
+            var listOfAlerts = []
+            snap.forEach(function(child){
+                data = child.val()
+                if (data.in_line){
+                    console.log(Object.keys(data.in_line))
+                    if (Object.keys(data.in_line).indexOf(firebase.auth().currentUser.uid) !== -1){
+                        listOfItems.push(data);
+                        console.log(listOfItems)
+                    }
                 }
-
-              });
-
-          }
-          else {
-              Alert.alert(
-                  'Uh oh!',
-                  "Looks like that line doesn't exist. Try again!",
-                  [
-                      {text: 'OK'},
-                  ],
-                  { cancelable: false }
-              )
-          }
-      });
-  }
-
+                if (data.up_next){
+                    if (Object.keys(data.up_next).indexOf(firebase.auth().currentUser.uid) !== -1){
+                        listOfAlerts.push(data);
+                    }
+                }
+            });
+            ctx.setState({
+                data: listOfItems,
+                alerts: listOfAlerts
+            })
+        });
+    }
     componentDidMount() {
+
         if (!this.state.signedIn){
             firebase.auth().signInAnonymously().catch(function(error) {
-              // Handle Errors here.
-              var errorCode = error.code;
-              var errorMessage = error.message;
-              // ...
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // ...
             });
         }
+        this.listenForItems();
+
     }
     render() {
         return (
             <View>
-
-                <View style={{width: Dimensions.get('window').width, padding: 25}}>
-                    <Text style={styles.head}>Have a code? Input it here to get in line! </Text>
-                    <TextInput
+                <ScrollView style={{width: Dimensions.get('window').width, padding: 25}}>
+                    <FlatList
+                    data={this.state.alerts}
+                    renderItem={({item}) => <ReadyLine data={item} joinLine={this.joinLine.bind(this)} />}
+                    />
+                    <Text style={styles.head}>Have a code? Input it here to get in line!</Text>
+                    <View style={{padding: 5}}>
+                        <TextInput
                         style={{height: 40, backgroundColor: '#fff', padding: 5}}
-                        placeholder="Enter your line code"
+                        placeholder="Enter line code"
                         onChangeText={(lineCode) => this.setState({lineCode})}
                         value={this.state.lineCode}
-                    />
+                        />
+                    </View>
 
                     <Button
-                        onPress={() => this.joinLine(this.state.lineCode, this.state)}
-                        title="Let's Go!"
-                        color="#4CAF50"
+                    onPress={() => this.joinLine(this.state.lineCode, this.state)}
+                    title="Let's Go!"
+                    color="#4CAF50"
                     />
-                </View>
+
+                    <Text style={styles.title}>Your Lines</Text>
+                    <FlatList
+                    data={this.state.data}
+                    renderItem={({item}) => <Line data={item} />}
+                    />
+
+                </ScrollView>
             </View>
 
         );
@@ -95,54 +167,14 @@ export default class LinesScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  justifyContent: 'flex-end',
-  alignItems: 'center',
-},
-map: {
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-},
     head: {
         padding: 10,
         textAlign: 'center',
         fontSize: 15
     },
-    top: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        flex: 1
-    },
-    select: {
-        backgroundColor: '#FFFFFF',
+    title: {
         padding: 10,
-        flexDirection: 'row'
-    },
-    item: {
-        borderWidth: 1,
-        borderColor: '#d6d7da',
-    },
-
-  modalContent: {
-    backgroundColor: 'white',
-    padding: 22,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 4,
-    borderColor: 'rgba(0, 0, 0, 0.1)',
-  },
-  bottomModal: {
-    justifyContent: 'flex-end',
-    margin: 0,
-  },
+        textAlign: 'center',
+        fontSize: 25
+    }
 });
