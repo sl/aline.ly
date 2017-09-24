5fc9ebabbcb34dfc0c9df350d19d368a4792dda5
sl <tyu.sam@gmail.com>
2017-09-24 08:06:24 -0400

    First production build :)

--- server/src/util/twilio_util.js
+++ server/src/util/twilio_util.js
@@ -1,16 +1,21 @@
 const accountSid = 'ACdf3466047ba8aef288f4d2c272819e33';
-const authToken = 'bce66695ab2cf028cfaf341863cef424'; 
+const authToken = 'bce66695ab2cf028cfaf341863cef424';
 const accountnumber = '16179774253'; //your phone number from twilio
 
-var twilio = require('twilio');
-var client = new twilio(accountSid, authToken);
+const twilio = require('twilio');
+const client = new twilio(accountSid, authToken);
 
-function send_text(phonenumber, event_name, line_code){
+const sendAlertText = function(phonenumber, event_name, line_code) {
+  console.log('sending twilio message!');
   client.messages.create({
-    body: 'You\'re done waiting! Head straight to ' + event_name + 
-    '. Please confirm your ticket and you will recieve a color code that you must show to the organizers. ' + 
+    body: 'You\'re done waiting! Head straight to ' + event_name +
+    ' and show the colors on your screen to the organizer!' +
     'aline.ly/' + line_code,
     to: phonenumber,  // text sent to this number
-    from: accountnumber 
+    from: accountnumber
   });
-}
+};
+
+module.exports = {
+  sendAlertText
+};
